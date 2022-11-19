feabfe13ef4a54c25427a36895172cba520c4c34
dsypasit <ong.pasit@gmail.com>
2022-11-20 01:12:23 +0700

    gotest ch3

---
+++ minus/example_test.go
@@ -0,0 +1,10 @@
+package minus
+
+import "fmt"
+
+// Minus return minux between 2 args
+func ExampleMinus() {
+	fmt.Println(Minus(1, 2))
+	// Output:
+	// -1
+}
--- ticket/ticket_test.go
+++ ticket/ticket_test.go
@@ -3,64 +3,26 @@ package ticket
 import "testing"
 
 func TestTicketPrice(t *testing.T) {
-	t.Run("should return 0 when age is 0", func(t *testing.T) {
-		want := 0.0
+	tests := []struct {
+		name string
+		age  int
+		want float64
+	}{
+		{name: "should return 0 when age is 0", age: 0, want: 0.0},
+		{name: "should return 15 when age is 4", age: 4, want: 15.0},
+		{name: "should return 15 when age is 15", age: 15, want: 15.0},
+		{name: "should return 30 when age is 16", age: 16, want: 30.0},
+		{name: "should return 30 when age is 50", age: 50, want: 30.0},
+		{name: "should return 5 when age over 50", age: 51, want: 5.0},
+	}
 
-		got := Price(0)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := Price(tt.age)
 
-		if got != want {
-			t.Errorf("Price(0) = %f; want %f", got, want)
-		}
-
-	})
-	t.Run("should return 15 when age is 4", func(t *testing.T) {
-		want := 15.0
-
-		got := Price(4)
-
-		if got != want {
-			t.Errorf("Price(4) = %f; want %f", got, want)
-		}
-
-	})
-	t.Run("should return 15 when age is 15", func(t *testing.T) {
-		want := 15.0
-
-		got := Price(15)
-
-		if got != want {
-			t.Errorf("Price(15) = %f; want %f", got, want)
-		}
-
-	})
-	t.Run("should return 30 when age is 16", func(t *testing.T) {
-		want := 30.0
-
-		got := Price(16)
-
-		if got != want {
-			t.Errorf("Price(16) = %f; want %f", got, want)
-		}
-
-	})
-	t.Run("should return 30 when age is 50", func(t *testing.T) {
-		want := 30.0
-
-		got := Price(50)
-
-		if got != want {
-			t.Errorf("Price(50) = %f; want %f", got, want)
-		}
-
-	})
-	t.Run("should return 5 when age is 51", func(t *testing.T) {
-		want := 5.0
-
-		got := Price(51)
-
-		if got != want {
-			t.Errorf("Price(50) = %f; want %f", got, want)
-		}
-
-	})
+			if got != tt.want {
+				t.Errorf("Price(%v) = %v; want %v", tt.age, got, tt.want)
+			}
+		})
+	}
 }
